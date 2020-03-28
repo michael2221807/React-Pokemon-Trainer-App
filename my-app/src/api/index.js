7bebdca56fa753f4a3fde1c3ab7e85c6e3d34082
michael2221807 <michael2221807@gmail.com>
2020-03-28 04:25:06 -0400

    trytrytyrytty4sssss

--- my-app/src/api/index.js
+++ my-app/src/api/index.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 // })
 
 const api = axios.create({
-	baseURL: 'http://localhost:3000/api',
+	baseURL: '/api',
 })
 
 export const insertUser = payload => api.post(`/user`, payload)
