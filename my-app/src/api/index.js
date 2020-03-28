9a7418691b9f70321cad557385139c7496e75a73
michael2221807 <michael2221807@gmail.com>
2020-03-28 00:27:27 -0400

    try

--- my-app/src/api/index.js
+++ my-app/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://poke-csc309-app.herokuapp.com/api',
 })
 
 export const insertUser = payload => api.post(`/user`, payload)
