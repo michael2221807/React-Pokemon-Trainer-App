08921645346852802e1347b13cf07c299130fd64
michael2221807 <michael2221807@gmail.com>
2020-03-27 20:55:28 -0400

    nothing works

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 
 const mongoose = require('./db/mongoose')
 const userRouter = require('./routes/user-router')
+const session = require("express-session");
 
 const app = express()
 const apiPort = process.env.PORT || 3000
@@ -13,9 +14,10 @@ app.use(cors())
 app.use(bodyParser.json())
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+// })
+
 
 app.use('/api', userRouter)
 
