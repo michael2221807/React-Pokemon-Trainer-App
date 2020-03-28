7c2a937a150eb964638601cb6066e10f1b6009ea
michael2221807 <michael2221807@gmail.com>
2020-03-28 02:17:22 -0400

    trytrytyrytty4

--- my-app/src/api/index.js
+++ my-app/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = "https://poke-csc309-app.herokuapp.com"
+const baseUrl = "https://poke-csc309-app.herokuapp.com/api"
 
 const api = axios.create({
     baseURL: baseUrl
