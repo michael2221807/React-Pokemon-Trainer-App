455539316cda7fbdaf87f8b8d6bd899953326459
michael2221807 <michael2221807@gmail.com>
2021-12-16 08:08:07 +0800

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,20 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/Users'
+// const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/Users'
 
-mongoose
-    .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
-    .catch(e => {
-        console.error('Connection error', e.message)
-    })
+// mongoose
+//     .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+//     .catch(e => {
+//         console.error('Connection error', e.message)
+//     })
+
+const { mongoose } = require('mongodb');
+const uri = "mongodb+srv://pokemon:pokemon123@pokemon.ptw7j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new mongoose(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
 
 module.exports = { mongoose }  // Export the active connection.
